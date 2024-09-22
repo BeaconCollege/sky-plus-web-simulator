06186a9e005b3f91ba28c469eda0a7d0b8bbd4c0
BeaconCollege <149310556+BeaconCollege@users.noreply.github.com>
2024-09-22 16:44:31 +0100

    beacon communication s is now open

--- src/atoms/globalBoxState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil'
-
-export const globalBoxState = atom({
-  key: 'globalState',
-  default: {
-    bootTime: Date.now(),
-    lastInteractiveSearch: -1,
-  },
-})
--- src/atoms/index.ts
+++
@@ -1,5 +0,0 @@
-export { controlsState } from './controlsState'
-export { globalBoxState } from './globalBoxState'
-export { mainMenuState } from './mainMenuState'
-export { scheduleTimeState, timeState } from './timeState'
-export { tvLicenseState } from './tvLicenseState'
